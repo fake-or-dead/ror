76101667a6f90eac604815252751807c16935eca
Piya Pakdeechaturun <a.postzone@gmail.com>
2012-08-27 16:22:01 +0700

    initial ror

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ror::Application.config.secret_token = '7b4b5062c154f8b0e2a9d85cb0e656a1a0d1b19608d0f037d705afeac60ff3f4ea5bd80a09f165f3f9b952f868dc67b7260c4c6c6b7be0dc42bd6aa831e7c8b2'
